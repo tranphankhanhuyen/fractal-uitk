4845b767cba29fec78cfefa9bc205d3480ffd2cf
Uyen Tran <tranphankhanhuyen@gmail.com>
2019-08-06 04:37:58 -0700

    Add contact

--- addon/components/f-t/contact/component.js
+++ addon/components/f-t/contact/component.js
@@ -1,20 +1,60 @@
 import Component from '@ember/component';
 import layout from './template';
-import { action } from '@ember/object';
-
+import EmberObject, { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ContactComponent extends Component {
   layout = layout;
-  classNames = ['contact'];
+  classNames = ['ft-contact'];
   anchor = 'contact';
   reCaptchaSuccess = false;
-  contactForm = {
+  contactForm = EmberObject.create({
     contactName: null,
-    email: null
-  };
+    email: null,
+    affiliation: null,
+    text: null
+  });
+  modal = EmberObject.create({
+    open: false,
+    message: '',
+    sending: false
+  });
+
+  @service contact;
 
   @action
-  submit() {}
+  async submit() {
+    const contactName = this.get('contactForm.contactName');
+    const email = this.get('contactForm.email');
+    const text = this.get('contactForm.text');
+    let modalOpen = true;
+    let modalMessage = '';
+    let sending = false;
+    if (!contactName) {
+      modalMessage = 'Please enter your contact name';
+    } else if (!email) {
+      modalMessage = 'Please enter your contact email';
+    } else if (!text) {
+      modalMessage = 'Please enter your contact message';
+    } else {
+      sending = true;
+      modalMessage = 'Sending contact message...'
+    }
+
+    this.modal.setProperties({ open: modalOpen, message: modalMessage, sending });
+    if (sending) {
+      sending = false;
+
+      const response = await this.contact.sendMessage(this.contactForm);
+      if (response.status === 200 || response.status === 202) {
+        modalMessage = 'Message has been sent to Fractal.'
+      } else {
+        modalMessage = 'Not able to send message to Fractal.'
+      }
+  
+      this.modal.setProperties({ open: modalOpen, message: modalMessage, sending });
+    }
+  }
 
   @action
   onCaptchaResolved(reCaptchaResponse = {}) {
---
+++ addon/services/contact.js
@@ -0,0 +1,31 @@
+import Service from '@ember/service';
+import fetch from 'fetch';
+
+export default class ContactService extends Service {
+  host = 'https://fractal-services.herokuapp.com/v1/mail/send';
+  toEmail = 'uyen.tran@fractaltx.com';
+
+  async sendMessage(contact) {
+    try {
+      const payload = {
+        from: contact.email,
+        to: this.toEmail,
+        subject: 'Contact by ' + contact.contactName,
+        text: contact.text
+      };
+
+      let response = await fetch(this.host, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+      });
+
+      return Array.isArray(response) ? response.firstObject : response;
+    } catch (ex) {
+      return { status: 400 }
+    }
+  }
+}
