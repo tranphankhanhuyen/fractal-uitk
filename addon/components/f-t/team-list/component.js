84f955be1fe702d367137de7501bf4663299b338
Uyen Tran <tranphankhanhuyen@gmail.com>
2019-08-06 03:56:19 -0700

    Fix missing style

--- addon/components/f-t/team-list/component.js
+++ addon/components/f-t/team-list/component.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
+import layout from './template';
 
 export default class TeamListComponent extends Component {
+  layout = layout;
   classNames = ['ft-team-list'];
   defaultImageUrl = "/images/blank-profile-picture.png";
   teamGroups = null;
